b8f9467bdaf1707b94a808003b1e3129c6261de5
Cbecker1 <christopher-m-becker@uiowa.edu>
2015-10-16 18:56:07 -0500

    DormRoom Layout
    Added Bed, Trashcan, Desk, Chair, TV Stand, TV, ClosetDoors, EntryDoor,
    Mini Fridge, and Wall

---
+++ Assets/Collide.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // If the Collider2D component is enabled on the object we collided with
+        if (coll.collider == true)
+        {
+            // Disables the Collider2D component
+            coll.collider.enabled = false;
+        }
+    }
+}
