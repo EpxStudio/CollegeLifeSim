2dbffe8be0d7e4c5029f1415e8e53413fcab46b1
Ian Wold <ilwold@hotmail.com>
2015-10-16 18:09:22 -0500

    InventoryItem Content
    added content

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InventoryItem : MonoBehaviour {
+public abstract class InventoryItem
+{
+	public int Health;
+	public int Knowledge;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public int Cost;
 }
