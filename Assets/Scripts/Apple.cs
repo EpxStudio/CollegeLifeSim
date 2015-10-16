d31d7c0422fbd3fc3dbd7292386b0284be8c995d
SSworn <LeonCapGrund@gmail.com>
2015-10-16 18:43:44 -0500

    added Snacks
    added Sanwitch and Aplle to Snacks class

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : Snack {
+	public Apple(){
+		Cost = 2;
+		Health = 3;
+		Knowledge = 1;
+	}
+}
---
+++ Assets/Scripts/Sanwitch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sanwitch : Snacks {
+	public Sanwitch(){
+		Cost = 8;
+		Health = 20;
+	}
+
+
+}
