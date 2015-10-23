edc3b4ba78c37d1794b1f0772d77fcd384124396
ovickman96 <ovickman96@gmail.com>
2015-10-23 17:44:24 -0500

    AdvisorRoom, WallEntity added
    New scene, new class

---
+++ Assets/Scripts/WallEntity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallEntity : Entity
+{
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        isSolid = true;
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public void OnCollisionEntity(Entity other)
+    {
+        int xDir = Auxs.Sign(posX - other.posX);
+        int yDir = Auxs.Sign(posY - other.posY);
+
+    }
+}
