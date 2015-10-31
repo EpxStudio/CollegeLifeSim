230c54a12c9511866ccb2ab254bf7a929210a4a0
Joseph Leiferman <joseph.leiferman@gmail.com>
2015-10-31 08:23:18 -0500

    added advisor entity script

---
+++ Assets/Scripts/AdvisorEntity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvisorEntity : Entity
+{
+	public override void OnAwake()
+	{
+		base.OnAwake();
+		
+		isSolid = true;  // the npc is a solid object. player can't move through it
+	}
+	
+	public override void OnUpdate()
+	{
+		base.OnUpdate();
+	}
+	
+	public override bool OnCollisionSolid(Entity other)
+	{
+
+		ChatController.Show ("Hello welcome to college, I will be you're advisor");
+		return true;
+
+	}
+
+}
