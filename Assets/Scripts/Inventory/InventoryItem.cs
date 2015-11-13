4d1893b04d174e9f8cd78ae0d6e3f0a0a5f6950d
Ian Wold <ilwold@hotmail.com>
2015-11-13 17:43:44 -0600

    Cleared more inventory bugs

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public abstract class InventoryItem
 {	
 	//boolean to show if said item is passive or active (used once then gone, i.e. not permament)
-	public boolean PermanentBoost;
+	public bool PermanentBoost;
 	public int Experience;
 	public int Knowledge;
 	public int Hunger;
