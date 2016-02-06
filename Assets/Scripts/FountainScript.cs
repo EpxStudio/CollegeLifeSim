3d5dc0b5921917e332579f690d4c10f3b67b870d
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-02-06 15:56:40 -0600

    Added functionality to scene
    Created two scripts for scenes tv’s and fountain

---
+++ Assets/Scripts/FountainScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FountainScript : Entity
+{
+	public override void OnAwake()
+	{
+		base.OnAwake();
+
+		isSolid = true;  // the npc is a solid object. player can't move through it
+	}
+
+	public override void OnUpdate()
+	{
+		base.OnUpdate();
+	}
+
+	public override bool OnCollisionSolid(Entity other)
+	{
+
+		ChatController.Show ("Would you like to make a wish?");
+		return true;
+
+	}
+
+}
---
+++ Assets/Scripts/TVScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVScript : Entity
+{
+	public override void OnAwake()
+	{
+		base.OnAwake();
+
+		isSolid = true;  // the npc is a solid object. player can't move through it
+	}
+
+	public override void OnUpdate()
+	{
+		base.OnUpdate();
+	}
+
+	public override bool OnCollisionSolid(Entity other)
+	{
+
+		ChatController.Show ("Today on Dino News!!!");
+		return true;
+
+	}
+
+}
