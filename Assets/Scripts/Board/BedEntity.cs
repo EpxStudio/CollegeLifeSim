11f5a1107bce9792098bd630928be8d8b670e9f7
paulyon <paulyon123@gmail.com>
2016-02-07 03:15:00 -0600

    Added a big chair 2x2

---
+++ Assets/Scripts/Board/BedEntity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BedEntity : Entity
+{
+    public override void OnAwake()
+    {
+        base.OnAwake();
+
+//		DialogueController.instance.Load(this, "npc_bed");
+
+        isSolid = true;  // the npc is a solid object. player can't move through it
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnAction()
+    {
+        base.OnAction();
+
+		ChatController.instance.SetDialogue(this);
+    }
+}
