1c4edc06bcd2b351ff1aa4aeaf3cca0b05739a95
andrewwillette <andrew-willette@uiowa.edu>
2016-02-13 15:57:42 -0600

    Began desk collision
    Desk Collision work

---
+++ Assets/ShopDeskScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopDeskScript : Entity {
+
+    public override bool OnCollisionSolid(Entity other)
+    {
+        return false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        CalculusTextbook myBook = new CalculusTextbook();
+        MainPlayer.Inventory.Add(myBook);
+        Debug.Log("Added new Calc Book");
+    }
+}
